a6aa9c4723dd03196f618794d624c45ed4fe4f97
Ross10 <itai.russo@yahoo.com>
2017-05-06 17:23:54 +0300

    get the current date when clicking on a date on the calendar and add an event to this date

--- app/src/main/java/com/example/ross/monitorbaby/setted_tasks_Activity.java
+++ app/src/main/java/com/example/ross/monitorbaby/setted_tasks_Activity.java
@@ -3,28 +3,70 @@ package com.example.ross.monitorbaby;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Calendar;
 import java.util.Date;
 
-public class setted_tasks_Activity extends AppCompatActivity {
+public class setted_tasks_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button fteeth,fcrawl,fstand,fwalk,ftalk,fliketalk,flaugh,fsmile,fgames;
+    private String dataToSet;
+    private int currentYear,currentMonth,currentDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setted_tasks_);
 
-        int[] dates = CalendarEvent.getDate(); // static function in Calendar event
-        int currentDay=dates[0];
-       int currentMonth =dates[1];
-       int currentYear =dates[2];
+        fteeth = (Button)findViewById(R.id.fteeth);
+        fstand = (Button)findViewById(R.id.standing);
+        fgames = (Button)findViewById(R.id.games);
 
-    String sa = "hi bro";
+
+        int[] dates = CalendarEvent.getDate(); // static function in Calendar event
+        currentDay=dates[0];
+       currentMonth =dates[1];
+       currentYear =dates[2];
+
+        fteeth.setOnClickListener(this);
+        //fcrawl.setOnClickListener(this);
+        fstand.setOnClickListener(this);
+       // fwalk.setOnClickListener(this);
+       // ftalk.setOnClickListener(this);
+      //  fliketalk.setOnClickListener(this);
+      //  flaugh.setOnClickListener(this);
+      //  fsmile.setOnClickListener(this);
+        fgames.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
         Intent intent = new Intent();
         intent.putExtra("currentDay", currentDay);
         intent.putExtra("currentMonth", currentMonth);
         intent.putExtra("currentYear", currentYear);
-        intent.putExtra("name", sa);
+
+
+        switch(v.getId()){
+            case R.id.fteeth:
+                dataToSet = "שן ראשונה";
+                break;
+            case R.id.standing:
+                dataToSet = "עמידה ראשונה";
+                break;
+            case R.id.games:
+                dataToSet = "משחק ראשון";
+                break;
+
+
+
+
+        }
+        intent.putExtra("name", dataToSet);
         setResult(RESULT_OK, intent);
         finish();
     }
